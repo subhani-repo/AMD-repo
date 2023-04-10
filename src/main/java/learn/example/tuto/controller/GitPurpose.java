33c171faa4699d8466dc36a6520b6fc6f75fa373
subhani <subani.m@apcfss.in>
2023-04-10 15:49:33 +0530

    tt

--- src/main/java/learn/example/tuto/controller/GitPurpose.java
+++ src/main/java/learn/example/tuto/controller/GitPurpose.java
@@ -3,7 +3,8 @@ package learn.example.tuto.controller;
 public class GitPurpose {
 public static void main(String[] args) {
 	//test 1 test this againnn	
-         //test 2 this and that
+
+         //test 2 that and this
 
 }
 }
